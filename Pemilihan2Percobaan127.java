b287b45375845cd6f514d9276ee8dab9d5600133
siskanuriaprilia <aprilias878@gmail.com>
2023-10-05 10:53:16 +0700

    Pemilihan2Percobaan127

---
+++ Pemilihan2Percobaan127.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class Pemilihan2Percobaan127{
+
+    public static void main (String[] args) {
+         Scanner input27 = new Scanner (System.in);
+
+        System.out.println("Masukkan Tahun"); 
+        int tahun = input27.nextInt();
+
+        if ((tahun % 4) == 0) {
+           if ((tahun % 100)!=0)
+            System.out.println("Tahun Kabisat"); 
+        } else 
+         System.out.println("Bukan Tahun Kabisat");
+    }      
+}
