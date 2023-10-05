5aa334a91261c03f426ff322413f77ebd6aa3b1e
siskanuriaprilia <aprilias878@gmail.com>
2023-10-05 14:10:12 +0700

    Pemilihan2Percobaan327

--- Pemilihan2Percobaan327.java
+++ Pemilihan2Percobaan327.java
@@ -1,41 +1,56 @@
 import java.util.Scanner;
 
-import javax.swing.plaf.synth.SynthOptionPaneUI;
-
 public class Pemilihan2Percobaan327 {
-
-    public static void main (String[] args) {
-        Scanner input27 = new Scanner (System.in);
+    public static void main(String[] args) {
+        Scanner input27 =new Scanner(System.in);
 
         String kategori;
         int penghasilan, gajiBersih;
         double pajak = 0;
 
-        System.out.println("Masukkan kategori : ");
-        kategori = noAbsen.nextLine();
-        System.out.println("Masukkan Besanya Penghasilan : ");
-        penghasilan = noAbsen.nextLine();
+        System.out.println("Masukkan Kategori : ");
+        kategori = input27.nextLine();
+        System.out.println("Masukkan Besarnya Penghasilan : ");
+        penghasilan = input27.nextInt();
 
-        if (kategori.equalsIgnoreCase("pekerjaan")) {
+        if (kategori.equalsIgnoreCase("pekerja")) 
+        {
             if (penghasilan <= 2000000)
+            {
                 pajak = 0.1;
+            }
             else if (penghasilan <= 3000000)
+            {
                 pajak = 0.15;
-            else
-                pajak = 0.2;
-            gajiBersih = (int) (penghasilan - (pajak * penghasilan));
-             System.out.println("Penghasil Bersih : " + gajiBersih);
-        } else if (kategori.equalsIgnoreCase("pebisnis"));
-            if  (penghasilan <= 2500000) 
-                 pajak = 0.15;
-            else if (penghasilan <= 3500000)
-                 pajak = 0.2;
+            }
             else 
-                 pajak = 0.25;
+            {
+            pajak = 0.2;
             gajiBersih = (int) (penghasilan - (pajak * penghasilan));
             System.out.println("Penghasil Bersih : " + gajiBersih);
-    }else
-            System.out.println("Masukkan Kategori Salah ");
+            }
+        } 
+        else if (kategori.equalsIgnoreCase("pebisnis")) 
+        {
+            if (penghasilan <= 2500000)
+            {
+            pajak = 0.15;
+            }
+            else if (penghasilan <= 3500000)
+            {
+            pajak = 0.2;
+            }
+            else 
+            {
+            pajak = 0.25;
+            gajiBersih = (int) (penghasilan - (pajak * penghasilan));
+            System.out.println("Penghasilan Bersih : " + gajiBersih);
+            } 
         }
+        else{
+            System.out.println("Masukan Kategori Salah ");
+            }
+            
+    }
 
 }
