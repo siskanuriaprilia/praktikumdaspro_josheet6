5049ce0ca1b2baab712ae3d91d08642918870a47
siskanuriaprilia <aprilias878@gmail.com>
2023-10-05 12:45:13 +0700

    Pemilihan2Percobaan227

--- Pemilihan2Percobaan227.java
+++ Pemilihan2Percobaan227.java
@@ -4,19 +4,26 @@ public class Pemilihan2Percobaan227 {
     
     public static void main (String[] args) {
         Scanner input27 = new Scanner (System.in);
-        
-        System.out.println("Masukkan sudut1 == 90");
-        System.out.println("Masukkan sudut2 == 90");
-        System.out.println("Masukkan sudut3 == 90");
+
+        float totalSudut;
+
+        System.out.println("Masukkan sudut1 :");
+        float sudut1 =input27.nextFloat();
+        System.out.println("Masukkan sudut2 :");
+        float sudut2 =input27.nextFloat();
+        System.out.println("Masukkan sudut3 :");
+        float sudut3 =input27.nextFloat();
 
         totalSudut = sudut1 + sudut2 + sudut3;
-        
-        if (totalSudut == 180) {
-            if ((sudut1 == 90)) ||(sudut2 == 90) || (sudut3 == 90) 
-               System.out.println("Segitiga tersebut adalah segitiga siku-siku");
+        System.out.println("total sudut yang dihasilkan : " + totalSudut);
+
+        if (totalSudut == 180){
+            if ((sudut1 == 90)  || (sudut2 == 90) || (sudut3 == 90)){
+                System.out.println("Segitiga tersebut adalah segitiga siku-siku");} 
+            else{
+                System.out.println("Segitiga tersebut adalah bukan segitiga siku-siku");}
+            }
             else
-               System.out.println("Segitiga tersebut adalah bukan siku-siku");
-        }else
-            System.out.println("Bukan Segitiga");
-}
-}
+            {System.out.println("Bukan Segitiga");}
+        }
+    }
